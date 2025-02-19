b4fde2deff33a3ef383b6653e119851708286b9e
Deep3k <141223129+Deep3k@users.noreply.github.com>
2025-02-19 23:54:54 +0530

    users.sql

--- users.sql
+++ users.sql
@@ -2,10 +2,7 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 19, 2025 at 07:11 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
